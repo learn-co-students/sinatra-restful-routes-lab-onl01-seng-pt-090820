5a942a193bc56b453adee194c1d3b94182d8107e
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-12-03 15:51:27 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/new' do
-
+    erb :new
   end
 
   post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
   end
 
   get '/recipes/:id' do
@@ -24,12 +26,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(id: params[:id])
+    erb :edit
   end
 
   patch '/recipes/:id' do
-  end
-
-  put '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "/recipes/#{recipe.id}"
   end
 
   delete '/recipes/:id' do
