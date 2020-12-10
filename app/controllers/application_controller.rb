2d14f67237f1d79e2c949fa072c53f5c416c0ab1
DavidRMorphew <morphewd@umich.edu>
2020-12-09 21:03:41 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,34 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    recipe = Recipe.find_by(id: params[:id])
+    recipe.update(params[:recipe])
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    recipe = Recipe.find_by(id: params[:id])
+    recipe.destroy
+    redirect "/recipes"
+  end
 end
