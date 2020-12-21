dec54ec05a388a5110203e50ed57241155de52f8
Cameron Hudson <camnn3@yahoo.com>
2020-12-21 01:55:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
   
-    post '/recipes' do
+  post '/recipes' do
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     redirect "/recipes/#{@recipe.id}"
   end
